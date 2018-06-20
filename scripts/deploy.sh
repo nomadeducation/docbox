67151aaabb3208818df8ab08dd6826ceeaed12fa
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2018-06-20 16:21:56 +0200

    chore(travis): specify the upstream branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-SSH_REPO="git@github.com:nomadeducation/kane-docs-build.git"
-SHA=`git rev-parse --verify HEAD`
-TARGET_BRANCH="master"
+SHA=$(git rev-parse --verify HEAD)
 
+rm -rf .git
 git init
-git remote add origin $SSH_REPO
+git remote add upstream git@github.com:nomadeducation/kane-docs-build.git
 
 git config user.name "Travis CI"
 git config user.email "developer+travis@nomadeducation.fr"
@@ -15,7 +14,7 @@ git add --force CNAME index.html bundle.js css/ images/
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 chmod 600 deploy_key
-eval `ssh-agent -s`
+eval $(ssh-agent -s)
 ssh-add deploy_key
 
-git push --force $SSH_REPO $TARGET_BRANCH
+git push --force upstream master
