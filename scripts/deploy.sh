14cab70d6e6d07a04c4efb61908b2b493624b67b
thomas <ster.bzh@gmail.com>
2018-07-21 14:57:39 +0200

    chore: re-bump repo name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ SHA=$(git rev-parse --verify HEAD)
 
 rm -rf .git
 git init
-git remote add upstream git@github.com:nomadeducation/kane-docs-site.git
+git remote add upstream git@github.com:nomadeducation/kane-docs-website.git
 
 git config user.name "Travis CI"
 git config user.email "developer+travis@nomadeducation.fr"
